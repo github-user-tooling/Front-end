5a842797b7a55669d9898e1f2602cc538de4d1e0
Michael Nunes <miketnunes@gmail.com>
2019-11-18 20:45:33 -0800

    Add user login state to store & integrate with PrivateRoute

--- src/components/utils/PrivateRoute.js
+++ src/components/utils/PrivateRoute.js
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 import { axiosWithAuth } from './axiosWithAuth';
 
+import { loginUser, logoutUser } from "../../redux/actions/userActions";
+
+
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const [isLogged, setIsLogged] = useState(null);
+  const [isLogged, setIsLogged] = useState(rest.userLogin.isLogged || null);
 
   useEffect(() => {
-    axiosWithAuth()
-      .get('https://staging-master-5ton9t2hfmasnxc.herokuapp.com/auth/active')
-      .then(res => setIsLogged(true))
-      .catch(err => setIsLogged(false));
+    if (rest.userLogin.isLogged === null || !rest.userLogin.isLogged) {
+      axiosWithAuth()
+        .get('https://staging-master-5ton9t2hfmasnxc.herokuapp.com/auth/active')
+        .then(res => {
+          setIsLogged(true)
+          rest.loginUser();
+        })
+        .catch(err => {
+          setIsLogged(false);
+          rest.logoutUser();
+        });
+    }
   }, []);
 
   //Fancy loading spinner could be placed here.
@@ -25,4 +38,14 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     />
   )
 }
-export default PrivateRoute;
+
+const mapStateToProps = state => ({
+  userLogin: state.User.userLogin
+});
+
+const mapActionsToProps = {
+  loginUser,
+  logoutUser
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(PrivateRoute);
--- src/redux/constants.js
+++ src/redux/constants.js
@@ -3,7 +3,9 @@ const constants = {
   CHART_CONSTANT: 'data:get-chart-data',
   DETAILCARD_CONSTANT: 'data:get-userdetail-data',
   GET_DASHBOARD_DATA: 'data:get-dashboard-data',
-  GET_SEARCH_QUERY: 'data:search-query'
+  GET_SEARCH_QUERY: 'data:search-query',
+  LOGIN_USER: 'user:login-user',
+  LOGOUT_USER: 'user:logout-user'
 };
 
 export default constants;
