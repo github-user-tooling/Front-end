d8f956fcbb0670e7d013e1b0d98a3f4b4305a1fc
Matt <matthew.endicott46@gmail.com>
2019-11-19 10:50:02 -0700

    dashboard mvp level complete still needs to be optomized // cleaned // styles // responive

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles/rootStyles/App.scss";
 // Pages
 import Dashboard from "./pages/dashboard/Dashboard";
-<<<<<<< HEAD
-// Components 
-=======
+// Components
 import Login from "./pages/login/Login";
 // Components
-import Demo from "./components/demo/Demo";
->>>>>>> 5b865094124f2e95636b14be8a48948923211c4f
 
 function App() {
   return (
     <div className="App">
-      <Router> 
+      <Router>
         <Switch>
           <Route path="/dashboard" component={Dashboard} />
         </Switch>
--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -7,7 +7,7 @@ export const dashboardData = () => dispatch => {
     .get("https://git-user-breakdown.herokuapp.com/user/dashboard", {
       withCredentials: true
     })
-    .then(res => { 
+    .then(res => {
       dispatch({
         type: constants.GET_DASHBOARD_DATA,
         payload: res.data
@@ -16,14 +16,13 @@ export const dashboardData = () => dispatch => {
     .catch(err => console.log(err));
 };
 
-<<<<<<< HEAD
-=======
-
-export const chartAction = (userID) => dispatch => {
+export const chartAction = userID => dispatch => {
   axios
-    .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/calendar`)
+    .get(
+      `https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/calendar`
+    )
     .then(res => {
-        console.log(res)
+      console.log(res);
       dispatch({
         type: constants.CHART_CONSTANT,
         payload: res
@@ -32,11 +31,13 @@ export const chartAction = (userID) => dispatch => {
     .catch(err => console.log(err));
 };
 
-export const tendenciesAction = (userID) => dispatch => {
+export const tendenciesAction = userID => dispatch => {
   axios
-    .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/tendencies`)
+    .get(
+      `https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/tendencies`
+    )
     .then(res => {
-        console.log(res)
+      console.log(res);
       dispatch({
         type: constants.GET_TENDENCIES,
         payload: res
@@ -45,12 +46,13 @@ export const tendenciesAction = (userID) => dispatch => {
     .catch(err => console.log(err));
 };
 
-
-export const detailCardAction = (userID) => dispatch => {
+export const detailCardAction = userID => dispatch => {
   axios
-    .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/profile`)
+    .get(
+      `https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/profile`
+    )
     .then(res => {
-        console.log(res)
+      console.log(res);
       dispatch({
         type: constants.DETAILCARD_CONSTANT,
         payload: res
@@ -59,95 +61,6 @@ export const detailCardAction = (userID) => dispatch => {
     .catch(err => console.log(err));
 };
 
-// For user on dashboard
-export const dashboardData = () => dispatch => {
-  const navDemoData = {
-    user: {
-      login: "PCDSandwichMan",
-      name: "Matt"
-    },
-    following: [
-      {
-        id: "MDQ6VXNlcjUzMjExNTIz",
-        avatarUrl: "https://avatars1.githubusercontent.com/u/53211523?v=4",
-        login: "Edwin-Chajon",
-        name: "Edwin Chajon",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjUzMjExNTIz",
-        avatarUrl: "https://avatars1.githubusercontent.com/u/53211523?v=4",
-        login: "Edwin-Chajon",
-        name: "Edwin Chajon",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjUzMjExNTIz",
-        avatarUrl: "https://avatars1.githubusercontent.com/u/53211523?v=4",
-        login: "Edwin-Chajon",
-        name: "Edwin Chajon",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjUzMjExNTIz",
-        avatarUrl: "https://avatars1.githubusercontent.com/u/53211523?v=4",
-        login: "Edwin-Chajon",
-        name: "Edwin Chajon",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjUzMjExNTIz",
-        avatarUrl: "https://avatars1.githubusercontent.com/u/53211523?v=4",
-        login: "Edwin-Chajon",
-        name: "Edwin Chajon",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjE2MDU5OTA3",
-        avatarUrl: "https://avatars3.githubusercontent.com/u/16059907?v=4",
-        login: "Suffyan-T",
-        name: "Suffyan Tariq",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjU1ODUxMzc0",
-        avatarUrl: "https://avatars2.githubusercontent.com/u/55851374?v=4",
-        login: "DustinG98",
-        name: "Dustin Graham",
-        followersCount: 2,
-        followingCount: 10
-      },
-      {
-        id: "MDQ6VXNlcjU0MDkyNTA5",
-        avatarUrl: "https://avatars2.githubusercontent.com/u/54092509?v=4",
-        login: "dijahdeen",
-        name: "Dijah Deen",
-        followersCount: 2,
-        followingCount: 10
-      }
-    ]
-  };
-
-  dispatch({
-    type: constants.GET_DASHBOARD_DATA,
-    payload: navDemoData
-  });
-  // axios.get('').then(res => {
-  //   dispatch({
-  //     type:,
-  //     payload:
-  //   })
-  // }).catch(err => console.log(err))
-
-};
-
->>>>>>> 5b865094124f2e95636b14be8a48948923211c4f
 export const searchUser = queryData => dispatch => {
   axios
     .get(`https://git-user-breakdown.herokuapp.com/search/${queryData}`, {
@@ -177,7 +90,6 @@ export const getUserDetails = userId => dispatch => {
 export const favoriteUser = userId => dispatch => {
   console.log(userId);
   axios
-<<<<<<< HEAD
     .post(
       `https://staging-master-5ton9t2hfmasnxc.herokuapp.com/follow/${userId}`,
       {},
@@ -185,9 +97,6 @@ export const favoriteUser = userId => dispatch => {
         withCredentials: true
       }
     )
-=======
-    .get(`https://git-user-breakdown.herokuapp.com/unfollow${userId}`)
->>>>>>> 5b865094124f2e95636b14be8a48948923211c4f
     .then(res => {
       console.log(res.data);
     })
