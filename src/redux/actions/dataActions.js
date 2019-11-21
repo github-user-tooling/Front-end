82e5b42a144b0e1b0aafd5e87ba32d521b44537d
Michael Nunes <miketnunes@gmail.com>
2019-11-21 08:17:41 -0800

    Comment out console logs in dataActions

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -21,7 +21,7 @@ export const chartAction = userID => dispatch => {
   axiosWithAuth()
     .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/calendar`)
     .then(res => {
-      console.log(res)
+      // console.log(res)
       dispatch({
         type: constants.CHART_CONSTANT,
         payload: res.data
@@ -35,7 +35,7 @@ export const tendenciesAction = userID => dispatch => {
   axiosWithAuth()
     .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/tendencies`)
     .then(res => {
-      console.log(res)
+      // console.log(res)
       dispatch({
         type: constants.GET_TENDENCIES,
         payload: res.data
@@ -68,7 +68,7 @@ export const detailCardAction = userID => dispatch => {
   axiosWithAuth()
     .get(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/user/${userID}/profile`)
     .then(res => {
-      console.log(res)
+      // console.log(res)
       dispatch({
         type: constants.DETAILCARD_CONSTANT,
         payload: res.data
@@ -95,7 +95,7 @@ export const searchUser = queryData => dispatch => {
 
 // Used when client from dashboard to user details
 export const getUserDetails = userId => dispatch => {
-  console.log(userId);
+  // console.log(userId);
   dispatch({
     type: constants.SET_USER_ID,
     payload: userId
@@ -104,7 +104,7 @@ export const getUserDetails = userId => dispatch => {
 
 // Used to favorite the user from dashboard search
 export const favoriteUser = userId => dispatch => {
-  console.log(userId);
+  // console.log(userId);
   axios
     .post(
       `https://staging-master-5ton9t2hfmasnxc.herokuapp.com/follow/${userId}`,
@@ -114,7 +114,7 @@ export const favoriteUser = userId => dispatch => {
       }
     )
     .then(res => {
-      console.log(res.data);
+      // console.log(res.data);
     })
     .catch(err => {
       console.log(err.response);
@@ -122,7 +122,6 @@ export const favoriteUser = userId => dispatch => {
 };
 
 export const followUser = user => dispatch => {
-  console.log('followUser', user);
   axiosWithAuth()
     .post(`https://staging-master-5ton9t2hfmasnxc.herokuapp.com/follow/${user.id}`)
     .then(res => {
